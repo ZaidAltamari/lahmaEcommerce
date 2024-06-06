6bbd8ab117fa2ee30be6bcee7a91b69abf871ad8
Radwan Susan <radwansusan90@gmail.com>
2024-06-06 17:10:09 +0400

    Update next.config.js

--- next-food-ordering-app/next.config.js
+++ next-food-ordering-app/next.config.js
@@ -32,20 +32,11 @@ const config = {
 	async headers() {
 		return [
 			{
-				source: '/api/:path*',
+				source: '/:path*',
 				headers: [
-					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
 					{
-						key: 'Access-Control-Allow-Origin',
-						value: 'http://194.195.86.67/*',
-					},
-					{
-						key: 'Access-Control-Allow-Methods',
-						value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS',
-					},
-					{
-						key: 'Access-Control-Allow-Headers',
-						value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+						key: 'Upgrade-Insecure-Requests',
+						value: '0',
 					},
 				],
 			},
